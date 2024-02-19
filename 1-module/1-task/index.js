1166fb7f05d07b2b190c19aae90a4e1e0ad9fbc7
Mariya Kochanova <balalum@gmail.com>
2024-02-19 14:29:41 +0400

    решила задачу из модуля 0 и задачу 1 из модуля 1

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,3 +1,15 @@
 function factorial(n) {
-  // ваш код...
+  if (n === 0 || n === 1) {
+    return 1;
+  }
+  let result = 1;
+  for (let i = 2; i <= n; i++) {
+    result *= i;
+  }
+  return result;
 }
+
+console.log(factorial(0));
+console.log(factorial(1));
+console.log(factorial(3));
+console.log(factorial(5));
