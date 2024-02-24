1d0c13820cd88725ec6953ff679d67113f4d661c
Mariya Kochanova <balalum@gmail.com>
2024-02-24 21:49:27 +0400

    сократила код в задаче 3 модуль 1

--- 1-module/3-task/index.js
+++ 1-module/3-task/index.js
@@ -2,9 +2,6 @@ function ucFirst(str) {
   if (str === '') {
     return ''
   } else {
-    let firstLetter = str[0];
-    let firstLetterUp = firstLetter.toUpperCase();
-    let string = firstLetterUp + str.slice(1);
-    return string;
+    return str[0].toUpperCase() + str.slice(1);
   }
 }
