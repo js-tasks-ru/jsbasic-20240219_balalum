b346561db67aa9643d079010ac39e93026489689
Mariya Kochanova <balalum@gmail.com>
2024-03-12 22:51:29 +0400

    решила задачу 3 из модуля 4

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,24 @@
 function highlight(table) {
-  // ваш код...
+  for (let i = 1; i < table.rows.length; i++) {
+
+    let row = table.rows[i];
+
+    if (row.cells[3].dataset.available == 'true') {
+      row.classList.add('available');
+    } else if (row.cells[3].dataset.available == 'false') {
+      row.classList.add('unavailable');
+    } else if (!row.cells[3].hasAttribute('data-available')) {
+      row.hidden = true;
+    }
+
+    if (row.cells[2].textContent == 'm') {
+      row.classList.add('male');
+    } else if (row.cells[2].textContent == 'f') {
+      row.classList.add('female');
+    }
+
+    if (row.cells[1].textContent < 18) {
+      row.setAttribute('style', 'text-decoration: line-through')
+    }
+  }
 }
