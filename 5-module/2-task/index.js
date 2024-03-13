ce72b162b7f2226756b9db67ed1a2caa21dd40ec
Mariya Kochanova <balalum@gmail.com>
2024-03-13 17:46:01 +0400

    решила задачи 1 и 2 из модуля 5

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,3 +1,14 @@
 function toggleText() {
-  // ваш код...
+  let btn = document.querySelector('.toggle-text-button');
+  let text = document.getElementById('text');
+
+  text.hidden = false;
+
+  btn.addEventListener('click', () => {
+    if (text.hidden === true) {
+      text.hidden = false;
+    } else if (text.hidden === false) {
+      text.hidden = true;
+    }
+  });
 }
