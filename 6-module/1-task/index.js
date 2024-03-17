1d1bb935c63048875475c020487b34487540ebd6
Mariya Kochanova <balalum@gmail.com>
2024-03-17 20:41:24 +0400

    решила задачу 1 из модуля 6

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -1,18 +1,34 @@
-/**
- * Компонент, который реализует таблицу
- * с возможностью удаления строк
- *
- * Пример одного элемента, описывающего строку таблицы
- *
- *      {
- *          name: 'Ilia',
- *          age: 25,
- *          salary: '1000',
- *          city: 'Petrozavodsk'
- *      }
- *
- */
 export default class UserTable {
   constructor(rows) {
+    this.rows = rows;
+
+    this.render();
+    this.deleteStr();
+  }
+
+  render() {
+    this.elem = document.createElement('table');
+    this.elem.innerHTML = '<thead><tr><th>Имя</th><th>Возраст</th><th>Зарплата</th><th>Город</th><th></th></tr></thead><tbody></tbody>';
+
+    let tbody = this.elem.lastElementChild;
+
+    this.rows.forEach(function(item) {
+      let tr = document.createElement('tr');
+      tbody.append(tr);
+      tr.insertAdjacentHTML('beforeEnd', `<td>${item.name}</td><td>${item.age}</td><td>${item.salary}</td><td>${item.city}</td><td><button>X</button></td>`)
+    });
+
+    return this.elem;
+  }
+
+  deleteStr() {
+    this.elem.addEventListener('click', event => {
+      let btn = event.target.closest('button');
+      let str = event.target.closest('tr');
+
+      if(btn) {
+        str.remove()
+      }
+    })
   }
 }
